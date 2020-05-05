57b2ea67c720e226aff07716ab29d5e6583c588e
Emillia <hentaimasterpad@gmail.com>
2020-05-05 23:54:28 +0200

    hmmm

--- CommandHandler/CommandHandler.cpp
+++ CommandHandler/CommandHandler.cpp
@@ -7,20 +7,26 @@ private:
 public:
 	
 	void handleMessage(SleepyDiscord::Message message) {
-		if (!message.author.bot && message.startsWith(defaultPrefix)) {
-			printf("a command has ran");
-			if (message.content == (defaultPrefix + "test")) {
-				client.sendMessage(message.channelID, "this is just a simple test <@!" 
-					+ message.author.ID 
-					+ ">");
-			}
+		try {
+			if (!message.author.bot && message.startsWith(defaultPrefix)) {
+				printf("a command has ran");
+				if (message.content == (defaultPrefix + "test")) {
+					client.sendMessage(message.channelID, "this is just a simple test <@!"
+						+ message.author.ID
+						+ ">");
+				}
 
-			if (message.content == (defaultPrefix + "me")) {
-				client.sendMessage(message.channelID, "<@!"
-					+ message.author.ID + ">");
-			}
+				if (message.content == (defaultPrefix + "me")) {
+					client.sendMessage(message.channelID, "<@!"
+						+ message.author.ID + ">");
+				}
 
+			}
 		}
+		catch (const std::exception& e) {
+			printf(e.what);
+		}
+		
 	}
 
 public:
