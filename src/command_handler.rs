30a7a02ee8b5626d361c236ad85d252934261d0f
Emillia <hentaimasterpad@gmail.com>
2020-06-10 00:52:58 +0200

    end me

--- src/command_handler.rs
+++ src/command_handler.rs
@@ -23,16 +23,25 @@ impl EventHandler for Handler {
             use serenity::model::gateway::Activity;
             use serenity::model::user::OnlineStatus;
 
-            let mut rng = rand::thread_rng().gen_range(0, 10);
+            let mut rng = rand::thread_rng().gen_range(0, 11);
 
-            let status = OnlineStatus::DoNotDisturb;
+            let status_dnd = OnlineStatus::DoNotDisturb;
+            let status = OnlineStatus::Online;
+            let status_afk = OnlineStatus::Idle;
 
             match rng{
-                0 => ctx.set_presence(Some(Activity::playing("I'm an airplane!")), status),
+                0 => ctx.set_presence(Some(Activity::playing("I'm an airplane!")), status_dnd),
                 1 => ctx.set_presence(Some(Activity::playing("Stop the memes!")), status),
-                2 => ctx.set_presence(Some(Activity::playing("Why am I doing this?")), status),
-                3..=10 => ctx.set_presence(Some(Activity::playing("Help me!")), status),
-                _ => ctx.set_presence(Some(Activity::playing("If you see this presence the bot has errored")), status),
+                2 => ctx.set_presence(Some(Activity::playing("Why am I doing this?")), status_afk),
+                3 => ctx.set_presence(Some(Activity::playing("Help me!")), status),
+                4 => ctx.set_presence(Some(Activity::playing("Help me!")), status_dnd),
+                5 => ctx.set_presence(Some(Activity::playing("Why am I doing this?")), status),
+                6 => ctx.set_presence(Some(Activity::playing("Why am I doing this?")), status_dnd),
+                7 => ctx.set_presence(Some(Activity::playing("Help me!")), status_afk),
+                8 => ctx.set_presence(Some(Activity::playing("Stop the memes!")), status_dnd),
+                9 => ctx.set_presence(Some(Activity::playing("Stop the memes!")), status_afk),
+                10 => ctx.set_presence(Some(Activity::playing("I'm an airplane!")), status),
+                _ => ctx.set_presence(Some(Activity::playing("If you see this presence the bot has errored")), status_dnd),
             };
             
             // Note that array index 0 is 0-indexed, while index 1 is 1-indexed.
