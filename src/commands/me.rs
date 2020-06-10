9996113346085dbfd5dd0e5175c719549e15762b
Emillia <hentaimasterpad@gmail.com>
2020-06-10 02:33:54 +0200

    oops

--- src/commands/me.rs
+++ src/commands/me.rs
@@ -3,13 +3,17 @@ use serenity::{
     framework::standard::{macros::command, CommandResult},
     model::{prelude::Message}
 };
-
+use std::string::String;
 
 #[command]
 fn me(ctx: &mut Context, message: &Message) -> CommandResult{
     let _ = message.reply(&ctx.http, " Welcome to hell");
+
     let g = message.guild(&ctx.cache).unwrap();
-    println!("Processed command 'me' by user '{}#{}' ({}) in guild {} ({}) ", message.author.name, message.author.discriminator, message.author.id, &g.read().name, &g.read().id);
+    let width = 4;
+    let discrim = format!("{:0width$}", message.author.discriminator, width = width);
+
+    println!("Processed command 'me' by user '{}#{}' ({}) in guild {} ({}) ", message.author.name, discrim, message.author.id, &g.read().name, &g.read().id);
 
     Ok(()) 
 }
--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -14,9 +14,9 @@ fn ping(ctx: &mut Context, message: &Message) -> CommandResult {
 
     let g = message.guild(&ctx.cache).unwrap();
     let width = 4;
-    let s = format!("{:0width$}", message.author.discriminator, width = width);
+    let discrim = format!("{:0width$}", message.author.discriminator, width = width);
 
-    println!("Processed command 'ping' by user '{}#{}' ({}) in guild {} ({}) ", message.author.name, s, message.author.id, &g.read().name, &g.read().id);
+    println!("Processed command 'ping' by user '{}#{}' ({}) in guild {} ({}) ", message.author.name, discrim, message.author.id, &g.read().name, &g.read().id);
 
     Ok(())
 
