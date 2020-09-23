3926c92e8222cb48f65f6e10d6dcd215cdf70372
Emillia <hentaimasterpad@gmail.com>
2020-09-23 14:51:56 +0200

    update ping command

--- src/commands/general/ping.rs
+++ src/commands/general/ping.rs
@@ -1,15 +1,47 @@
 use serenity::{
     client::{Context},
     framework::standard::{macros::command, CommandResult},
-    model::prelude::Message
+    model::prelude::Message,
+    client::bridge::gateway::ShardId
 };
+use crate::ShardManagerContainer;
+
 
 #[command]
 #[description = "Checks Discord's API / message latency."]
 #[usage = "ping"]
 #[example = "ping"]
 fn ping(ctx: &mut Context, message: &Message) -> CommandResult {
-    let _ = message.channel_id.say(&ctx.http, "Pong!");
+    // The shard manager is an interface for mutating, stopping, restarting, and
+    // retrieving information about shards.
+    let data = &ctx.data.read();
+
+    let shard_manager = match data.get::<ShardManagerContainer>() {
+        Some(v) => v,
+        None => {
+            let _ = message.reply(&ctx.http, "There was a problem getting the shard manager");
+
+            return Ok(());
+        },
+    };
+
+    let manager = shard_manager.lock();
+    let runners = manager.runners.lock();
+
+    // Shards are backed by a "shard runner" responsible for processing events
+    // over the shard, so we'll get the information about the shard runner for
+    // the shard this command was sent over.
+    let runner = match runners.get(&ShardId(ctx.shard_id)) {
+        Some(runner) => runner,
+        None => {
+            let _ = message.reply(&ctx,  "No shard found");
+
+            return Ok(());
+        },
+    };
+
+
+    let _ = message.reply(&ctx, &format!("The shard latency is {:?}", runner.latency));
 
     let g = message.guild(&ctx.cache).unwrap();
     let width = 4;
--- src/main.rs
+++ src/main.rs
@@ -55,7 +55,7 @@ fn init_settings() -> Settings {
     toml::from_str(&contents).expect("Could not deserialize configuration")
 }
 
-struct ShardManagerContainer;
+pub struct ShardManagerContainer;
 
 impl TypeMapKey for ShardManagerContainer {
     type Value = Arc<Mutex<ShardManager>>;
