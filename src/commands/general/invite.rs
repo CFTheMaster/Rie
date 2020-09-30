4cf98736c9dc304684f5c7b624dbeb9cf9f28140
Emillia <hentaimasterpad@gmail.com>
2020-10-01 00:26:45 +0200

    make the invite a neat embed

--- src/commands/general/invite.rs
+++ src/commands/general/invite.rs
@@ -1,7 +1,8 @@
 use serenity::{
     client::Context,
     framework::standard::{macros::command, CommandResult},
-    model::{prelude::Message}
+    model::{prelude::Message},
+    utils::Colour
 };
 
 #[command]
@@ -9,7 +10,28 @@ use serenity::{
 #[usage = "invite"]
 #[example = "invite"]
 fn invite(ctx: &mut Context, message: &Message) -> CommandResult{
-    let _ = message.reply(&ctx.http, "invite me: https://discord.com/oauth2/authorize?client_id=706219430912327742&scope=bot&permissions=67160068");
+    if let Err(why) = message.channel_id.send_message(&ctx, |m| {
+        m.embed(|e| {
+            let width = 4;
+            let discrim = format!("{:0width$}", message.author.discriminator, width = width);
+            e.title("Rie Invite");
+            e.description(format!("[Click here for the invite](https://discord.com/oauth2/authorize?client_id=706219430912327742&scope=bot&permissions=67160068)"));
+            e.colour(Colour::new(6684876));
+            e.footer(|f| {
+                f.icon_url(message.author.avatar_url().unwrap());
+                f.text(format!("This command was executed by {}#{}", message.author.name, discrim));
+
+                f
+            });
+            e
+        });
+
+        m
+    }) {
+        println!("Error: {:?}", why);
+        let _ = message.channel_id.say(&ctx.http, "Missing permissions");
+    }
+
 
     let g = message.guild(&ctx.cache).unwrap();
     let width = 4;
