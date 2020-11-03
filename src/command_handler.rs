9bb78dc88a40f6bc669bd09cd6cd4d2bffd50075
Emillia <hentaimasterpad@gmail.com>
2020-11-03 02:14:09 +0100

    make it read the random status from .env

--- src/command_handler.rs
+++ src/command_handler.rs
@@ -1,4 +1,6 @@
-use rand::{Rng};
+use rand::{self, Rng}; 
+use dotenv::dotenv;
+use std::env;
 
 use serenity::{
     model::{gateway::Ready},
@@ -44,33 +46,33 @@ pub struct Moderation;
 #[commands(anime, hentai, dva, neko, nsfwneko, trap, yuri)]
 pub struct Images;
 
-
 pub struct Handler;
 impl EventHandler for Handler {
     fn ready(&self, ctx: Context, data: Ready){
+        dotenv().ok();
+
+        let pressence = env::var("PRESENCE_SETTER")
+            .expect("no pressence found in .env");
+
+        let split: Vec<&str> = pressence.split("__").collect();
         if let Some(shard) = data.shard {
             use serenity::model::gateway::Activity;
             use serenity::model::user::OnlineStatus;
 
-            let rng = rand::thread_rng().gen_range(0, 11);
+           let rng = rand::thread_rng().gen_range(0, 2);
 
             let status_dnd = OnlineStatus::DoNotDisturb;
             let status = OnlineStatus::Online;
             let status_afk = OnlineStatus::Idle;
 
+            let randomText = split[rand::thread_rng().gen_range(0, split.len())];
+
+
             // Make this into an array (WIP)
             match rng{
-                0 => ctx.set_presence(Some(Activity::playing("I'm an airplane!")), status_dnd),
-                1 => ctx.set_presence(Some(Activity::playing("Stop the memes!")), status),
-                2 => ctx.set_presence(Some(Activity::playing("Why am I doing this?")), status_afk),
-                3 => ctx.set_presence(Some(Activity::playing("Help me!")), status),
-                4 => ctx.set_presence(Some(Activity::playing("Help me!")), status_dnd),
-                5 => ctx.set_presence(Some(Activity::playing("Why am I doing this?")), status),
-                6 => ctx.set_presence(Some(Activity::playing("Why am I doing this?")), status_dnd),
-                7 => ctx.set_presence(Some(Activity::playing("Help me!")), status_afk),
-                8 => ctx.set_presence(Some(Activity::playing("Stop the memes!")), status_dnd),
-                9 => ctx.set_presence(Some(Activity::playing("Stop the memes!")), status_afk),
-                10 => ctx.set_presence(Some(Activity::playing("I'm an airplane!")), status),
+                0 => ctx.set_presence(Some(Activity::playing(randomText)), status_dnd),
+                1 => ctx.set_presence(Some(Activity::playing(randomText)), status),
+                2 => ctx.set_presence(Some(Activity::playing(randomText)), status_afk),
                 _ => ctx.set_presence(Some(Activity::playing("If you see this presence the bot has errored")), status_dnd),
             };
             
